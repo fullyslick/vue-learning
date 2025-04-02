4e9fabbf60f82c656ee383b94a32622adb97ce93
Aleksandar Rashkov <arashkov@aiopsgroup.com>
2025-04-02 09:57:38 +0300

    Component Props

---
+++ js/components/App.js
@@ -0,0 +1,8 @@
+import AppButton from "./AppButton.js";
+
+export default {
+  // Declares the components that will be used in the app
+  components: {
+    "app-button": AppButton,
+  },
+};
--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,8 +1,15 @@
 export default {
-  // Declares the template/markup that will be used by the app-button component
+  // the class prop is an object that contains all the classes that will be used
+  // this object is used to conditionally apply classes to the button
+  // the syntax is :class="{ 'class-name': condition }"
+  // note that :class can accept also an array or string
   template: `
     <button
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2 disabled:bg-gray-400"
+        :class="{
+            'font-bold py-2 px-4 rounded ml-2 disabled:bg-gray-400': true,
+            'bg-blue-500 hover:bg-blue-700 text-white' : buttonStyle === 'primary',
+            'bg-red-500 hover:bg-red-700 text-white' : buttonStyle === 'secondary',           
+        }"  
         :disabled="isDisabled"
         @click="toggleDisable">
         <!-- Slot is where the content will be injected -->
@@ -12,6 +19,18 @@ export default {
     return {
       isDisabled: false,
     };
+  },
+
+  // props are how we pass data from the parent component to the child component
+  // type is the type of data that is expected to be passed in
+  // default is the value that will be used if no value is passed in
+  // always use camelCase for prop names
+  // however when passing in the template use kebab-case
+  props: {
+    buttonStyle: {
+      type: String,
+      default: "primary",
+    },
   },
 
   methods: {
